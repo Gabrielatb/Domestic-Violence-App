0d57d5dee4c112e8f6169bfb4570dbfab5843fa5
vagrant <vagrant@vagrant.vm>
2018-05-08 21:11:47 +0000

    Grammatical Errors Corrected

--- static/js/login.js
+++ static/js/login.js
@@ -8,10 +8,10 @@ console.log(victimId);
 if (victimId === loginId){
 
     $(document).ready(function(){
-    alert("If at any point while visiting this site you feel \
-    unsafe: \n \n-Press the escape button on the top left to hide the \
-    screen screen  \n-Call our 24/7 hotline number: 1-800-799-7233 \n-If \
-    you feel you are in imminent danger call 911");
+    alert("If at any point while visiting this site you feel unsafe: \
+        \n \n- Press the escape button on the top left to hide the screen. \
+        \n- Call our 24/7 hotline number: 1-800-799-7233. \
+        \n- If you feel you are in imminent danger call 911.");
 
 });
 
